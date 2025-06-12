fcd4dd513bc71129b8ada5f6ee66ebab6379544f
Carlos Diez <cdiez@lksnext.com>
2025-06-12 15:23:32 +0200

    chore: Add Eclipse Foundation contributors to the license header

--- extensions/store/sql/migrations/src/main/resources/org/eclipse/tractusx/issuerservice/postgresql/migration/participantcontext/V0_0_1__Init_ParticipantContext.sql
+++ extensions/store/sql/migrations/src/main/resources/org/eclipse/tractusx/issuerservice/postgresql/migration/participantcontext/V0_0_1__Init_ParticipantContext.sql
@@ -1,5 +1,6 @@
 /*
  *  Copyright (c) 2025 Cofinity-X
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
  *
  *  This program and the accompanying materials are made available under the
  *  terms of the Apache License, Version 2.0 which is available at
