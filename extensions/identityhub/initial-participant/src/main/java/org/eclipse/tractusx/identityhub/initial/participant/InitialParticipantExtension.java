251ffd8675a8cc53981ade29001359e146643a40
Md. Wahidul Azam <wahidul.azam@technovativesolutions.co.uk>
2026-03-18 20:43:26 +0600

    Update extensions/identityhub/initial-participant/src/main/java/org/eclipse/tractusx/identityhub/initial/participant/InitialParticipantExtension.java
    Co-authored-by: Mathias Moser <mathias.moser@catena-x.net>

--- extensions/identityhub/initial-participant/src/main/java/org/eclipse/tractusx/identityhub/initial/participant/InitialParticipantExtension.java
+++ extensions/identityhub/initial-participant/src/main/java/org/eclipse/tractusx/identityhub/initial/participant/InitialParticipantExtension.java
@@ -1,6 +1,8 @@
 /*
 *   Copyright (c) 2025 LKS Next
 *   Copyright (c) 2025 Contributors to the Eclipse Foundation
+*   Copyright (c) 2026 Technovative Solutions 
+
 *
 *   See the NOTICE file(s) distributed with this work for additional
 *   information regarding copyright ownership.
